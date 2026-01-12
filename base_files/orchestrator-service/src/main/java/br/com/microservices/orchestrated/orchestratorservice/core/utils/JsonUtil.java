cd2a53d1b460d1cfc219eab2bb84f2d548efc1fe
Lucas Ezequiel <lucasbarrosez@gmail.com>
2026-01-11 23:05:03 -0300

    feat: implementar global exception handler e classes utilitarias para os serviços

---
+++ base_files/orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/utils/JsonUtil.java
@@ -0,0 +1,30 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.utils;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.dto.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper mapper;
+
+    public String toJson(Object object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting object to JSON", e);
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return mapper.readValue(json, Event.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON to Event", e);
+        }
+    }
+
+}
